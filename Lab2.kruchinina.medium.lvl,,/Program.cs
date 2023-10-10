ad05a13a04a04da85c04726bc33ab3d7c841e0cc
NIK <NIK@DESKTOP-MH7JFSC>
2023-10-10 22:43:01 +0200

    Add project files.

---
+++ Lab2.kruchinina.medium.lvl,,/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Write("Введите a:");
+double a = double.Parse(Console.ReadLine());
+Console.Write("Введите b:");
+double b = double.Parse(Console.ReadLine());
+Console.Write("Введите c:");
+double c = double.Parse(Console.ReadLine());
+if ((c * c) == (a * a) + (b * b)) Console.WriteLine("Прямоугольный");
+else Console.WriteLine("Не прямоугольный");
